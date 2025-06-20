84e01fc7a2326b090cdda8d754c391fae3f0dff8
asad315 <84276124+asadqureshi315@users.noreply.github.com>
2025-06-20 05:45:40 +0530

    Update recipe.route.js

--- backend/routes/recipe.route.js
+++ backend/routes/recipe.route.js
@@ -12,7 +12,7 @@ const Router = require("express").Router();
 Router.route("/get").get(readRecipe);
 Router.route("/get/:id").get(readSingleRecipe);
 Router.route("/add").post(upload.single("photo"), addRecipe);
-Router.route("/update").patch(upload.single("photo"), updateRecipe);
-Router.route("/delete").delete(deleteRecipe);
+Router.route("/update/:id").patch(upload.single("photo"), updateRecipe);
+Router.route("/delete/:id").delete(deleteRecipe);
 
 module.exports = Router;
